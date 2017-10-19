86deb38abef440c09462d1393f9bdfffc3d9e330
Michaela Blackham <Michaelablackham@gmail.com>
2017-10-18 19:56:00 -0600

    reducer test one - initial state

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -5,5 +5,17 @@ import {newGame, makeGuess, toggleInfoModal} from './actions';
 //wrap the entire reducer test within one describe fuction
 // beacause there is only one export in reducers.js
 describe('Reducer', () => {
-  it
+  it('should set the initital state as default for empty info', () =>{
+    const state = reducer(undefined, {type: '__UNKNOWN'});
+
+    //guesses should equal an empty array
+    expect(state.guesses).toEqual([]);
+    // feedback should equal 'Make your guess!'
+    expect(state.feedback).toEqual('Make your guess!');
+    //correct answer should equal >0 && <100
+    expect(state.correctAnswer).toBeGreaterThanOrEqual(0);
+    expect(state.correctAnswer).toBeLessThanOrEqual(100);
+    // show info modal should be === false
+    expect(state.showInfoModal).toEqual(false);
+  });
 });
