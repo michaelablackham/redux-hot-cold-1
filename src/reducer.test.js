ffa92453f25c8ec8c1b46612cfac288b53b5eeeb
Michaela Blackham <Michaelablackham@gmail.com>
2017-10-18 20:11:56 -0600

    basic reducer tests

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -2,8 +2,7 @@
 import reducer from './reducer';
 import {newGame, makeGuess, toggleInfoModal} from './actions';
 
-//wrap the entire reducer test within one describe fuction
-// beacause there is only one export in reducers.js
+//why do we wrap the entire thing in one describe? when it comes to state?
 describe('Reducer', () => {
   it('should set the initital state as default for empty info', () =>{
     const state = reducer(undefined, {type: '__UNKNOWN'});
@@ -17,5 +16,59 @@ describe('Reducer', () => {
     expect(state.correctAnswer).toBeLessThanOrEqual(100);
     // show info modal should be === false
     expect(state.showInfoModal).toEqual(false);
+  });
+
+  describe('newGame', () => {
+    it('should start new game', () => {
+      // fake info to pretend a current game is being played
+      let state = {
+        guesses: [2,6,9,22],
+        feedback: 'coolio!',
+        correctAnswer: -1 //Negative do different to new game
+        // I needed help with this one here --^
+      };
+      state = reducer(state, newGame());
+      //new game should have no guesses
+      expect(state.guesses).toEqual([]);
+      //new game feedback should equal 'Make your guess!'
+      expect(state.feedback).toEqual('Make your guess!');
+      //new game correct answer should equal >0 && <100
+      expect(state.correctAnswer).toBeGreaterThanOrEqual(0);
+      expect(state.correctAnswer).toBeLessThanOrEqual(100);
+      // show info modal should be === false
+      expect(state.showInfoModal).toEqual(false);
+    });
+  });
+
+  describe('makeGuess', () => {
+    it('should make a guess', () => {
+      let state = {
+        guesses: [],
+        feedback: 'Make your guess!',
+        correctAnswer: 72
+      }
+
+      state = reducer(state, makeGuess(14));
+      expect(state.guesses).toEqual([14]);
+    });
+  });
+
+  describe('toggleInfoModal', () => {
+    it('should show info modal', () => {
+      //set state to not show it, so we can turn it on
+      let state = {
+        showInfoModal: false
+      };
+      state = reducer(state, toggleInfoModal());
+      expect(state.showInfoModal).toEqual(true);
+    });
+
+    it('should hide info modal', () => {
+      let state = {
+        showInfoModal: true
+      };
+      state = reducer(state, toggleInfoModal());
+      expect(state.showInfoModal).toEqual(false);
+    });
   });
 });
